104c5a7296ba04afdde2bf241786d1b8bc399731
Vishwa <vishwasomarampet029@gmail.com>
2023-10-26 21:48:05 +0530

    New features added

--- script.js
+++ script.js
@@ -5,41 +5,85 @@ function getComputerChoice() {
   const random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
-
+let playing = true;
+let computerChoice;
+let playerChoice;
 let playerScore = 0;
 let computerScore = 0;
-// let roundWinner = "";
+
+const player = document.querySelector("#player-score");
+player.textContent = `Player : ${playerScore}`;
+
+const computer = document.querySelector("#computer-score");
+computer.textContent = `Computer : ${computerScore}`;
+
+const buttons = document.querySelectorAll(".btn");
+
+const output = document.querySelector(".score-info");
+output.textContent = "Choose your weapon";
+
+const msg = document.querySelector(".score-msg");
+msg.textContent = "First to score 5 points wins the game!";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerChoice = button.id.toUpperCase();
+    playGame();
+  });
+});
 
 function playround() {
-  let computerSelection = getComputerChoice().toUpperCase();
-  let playerSelection = prompt("Rock Or paper or Scissors").toUpperCase();
-  console.log(playerSelection);
-  console.log(computerSelection);
-  if (playerSelection === computerSelection) return "tie";
+  computerChoice = getComputerChoice().toUpperCase();
+  console.log(playerChoice);
+  console.log(computerChoice);
+  if (playerChoice === computerChoice) {
+    output.textContent = "It's a tie!";
+    msg.textContent = `${playerChoice} ties with ${computerChoice}`;
+  }
   if (
-    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
-    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
-    (playerSelection === "PAPER" && computerSelection === "ROCK")
+    (playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (playerChoice === "SCISSORS" && computerChoice === "PAPER") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK")
   ) {
     playerScore++;
-    return "player";
+    player.textContent = `Player : ${playerScore}`;
+
+    output.textContent = "You won!";
+    msg.textContent = `${playerChoice} beats ${computerChoice}`;
   } else if (
-    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
-    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
-    (computerSelection === "PAPER" && playerSelection === "ROCK")
+    (computerChoice === "ROCK" && playerChoice === "SCISSORS") ||
+    (computerChoice === "SCISSORS" && playerChoice === "PAPER") ||
+    (computerChoice === "PAPER" && playerChoice === "ROCK")
   ) {
     computerScore++;
-    return "computer";
+    computer.textContent = `Computer : ${computerScore}`;
+    output.textContent = "You lost!";
+    msg.textContent = `${playerChoice} is beaten by ${computerChoice}`;
   }
 }
 console.log(playround());
 
-function game() {
-  while (playerScore !== 5 || computerScore !== 5) {
-    console.log(playround());
-    if (playerScore === 5) return "Player Won!!";
-    else if (computerScore == 5) return "Computer Won!!";
+function playGame() {
+  if (playing) {
+    playround();
+    if (playerScore === 5) {
+      output.textContent = "You Won the Game! Congrats";
+      playing = false;
+    }
+    if (computerScore === 5) {
+      output.textContent = "You lost the Game";
+      playing = false;
+    }
   }
 }
 
-console.log(game());
+const reset = document.querySelector(".cta");
+reset.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  playing = true;
+  output.textContent = "Choose your weapon";
+  msg.textContent = "First to score 5 points wins the game!";
+  player.textContent = `Player : ${playerScore}`;
+  computer.textContent = `Computer : ${computerScore}`;
+});
